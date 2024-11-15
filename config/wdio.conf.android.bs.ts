f9cea8e0200241dfa12796ec1dfd995ad7865ecb
Olha Teplova <olitieplove@gmail.com>
2024-11-15 19:09:26 +0200

    fix: wdio.conf.bs.ts

--- config/wdio.conf.android.bs.ts
+++ config/wdio.conf.android.bs.ts
@@ -1,6 +1,7 @@
 import { config as sharedConfig } from "./wdio.conf.js";
 import dotenv from "dotenv";
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
+
 export const config = {
     ...sharedConfig,
     user: process.env.BROWSERSTACK_USERNAME,
@@ -8,33 +9,37 @@ export const config = {
     hostname: 'hub.browserstack.com',
 
     capabilities: [{
+        acceptInsecureCerts: true,
+        geoLocation: "PL",
+        timezone: "Warsaw",
+        'appium:app': process.env.BROWSERSTACK_ANDROID_APP_ID,
+        'appium:automationName': 'UiAutomator2',
         'bstack:options': {
-          deviceName: 'Samsung Galaxy S22 Ultra',
-          osVersion: '12.0',
-          deviceOrientation: 'portrait',
-          acceptInsecureCerts: true,
-          autoGrantPermissions: true,
-          geoLocation: "PL",
-          timezone: "Warsaw"
-        }
-      }],
+            deviceName: 'Samsung Galaxy S22 Ultra',
+            osVersion: '12.0',
+            deviceOrientation: 'portrait',
+            autoGrantPermissions: true,
+            // appiumVersion: "2.0.0", // Ensure compatibility
+        },
+    }],
+
     services: [
         [
-          'browserstack',
-          {
-            app: process.env.BROWSERSTACK_ANDROID_APP_ID,
-            buildIdentifier: "${BUILD_NUMBER}",
-            // opts: { forcelocal: false, localIdentifier: "webdriverio-appium-app-browserstack-repo" },
-            browserstackLocal: true,
-            testObservability: true,
-            testObservabilityOptions: { 
-                  projectName: "BrowserStack Android app testing",
-                  buildName: 'browserstack Android build',
-              },
-            debug: true,
-            networkLogs: true,
-            consoleLogs: "warn"
-          },
+            'browserstack',
+            {
+                app: process.env.BROWSERSTACK_ANDROID_APP_ID,
+                buildIdentifier: "${BUILD_NUMBER}",
+                browserstackLocal: true,
+                opts: { localIdentifier: "my-pipeline-local" },
+                testObservability: true,
+                testObservabilityOptions: { 
+                    projectName: "BrowserStack Android app testing",
+                    buildName: 'browserstack Android build',
+                },
+                debug: true,
+                networkLogs: true,
+                consoleLogs: "warn",
+            },
         ]
-      ],
-}
+    ],
+};
